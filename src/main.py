ff7d71c40924c33bf991002d8c9eb6bb6ae91fe7
Cory Maccarrone <darkstar6262@gmail.com>
2025-04-01 03:53:03 -0700

    Take console port on the command line.
    This also sets us up for being able to expose an HTTP server to operate
    the siren.

--- src/main.py
+++ src/main.py
@@ -11,10 +11,15 @@ PORT = 12345
 if __name__ == "__main__":
     from model.fs3t22a import FS3T22A
     from timer.af_timer import AFTimer, Mode, Button
+    import argparse
     import time
 
+    parser = argparse.ArgumentParser(description="AF Timer main runtime.")
+    parser.add_argument("--console_port", type=int, default=PORT, help="Port to run the console telnet service on")
+    args = parser.parse_args()
+
     af_timer = AFTimer(FS3T22A)
-    console = Console(HOST, PORT)
+    console = Console(HOST, args.console_port)
     locals = {
         'timer': af_timer,
         'siren': af_timer._siren,
